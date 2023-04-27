e7ed7f5de5f97eba42835f48e0e561ba540a854c
lpatyn <lautaropatyn@outlook.com>
2023-04-27 18:49:46 -0300

    Fixed some format issues in prompts file

--- resources/prompts.js
+++ resources/prompts.js
@@ -1,15 +1,12 @@
 module.exports = {
-    hola: `Hola, %NAME%, todo bien?`,
-    adios: `Nos vemos %NAME%, cuidate!`,
-    nihongo: `私はボットのMoockyです, よろしくお願いします！`,
-    comandos: `Hola! Soy Moocky, tu bot otaku de confianza.
-
-A continuación te dejaré una lista de comandos útiles que puedes enviarme:
-
-    !hola - Te recibiré como corresponde.
-    !adios - Si te vas, no olvides despedirte de mí!
-    !nihongo - Pues claro, estamos en una academia de japonés, o no?
-    !comandos - Te mostraré nuevamente este mensaje guía.
-    
-Saludos!`
+    hola: 'Hola, %NAME%, todo bien?',
+    adios: 'Nos vemos %NAME%, cuidate!',
+    nihongo: '私はボットのMoockyです, よろしくお願いします！',
+    comandos: 'Hola! Soy Moocky, tu bot otaku de confianza.\n' +
+    'A continuación te dejaré una lista de comandos útiles que puedes enviarme:\n\n' +
+    '!hola - Te recibiré como corresponde.\n' +
+    '!adios - Si te vas, no olvides despedirte de mí!\n' +
+    '!nihongo - Pues claro, estamos en una academia de japonés, o no?\n' +
+    '!comandos - Te mostraré nuevamente este mensaje guía.\n\n' +
+    'Saludos!'
 }
