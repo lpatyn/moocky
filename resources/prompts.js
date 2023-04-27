f1f8310fb03d2ea8a61a0a42338838407d62459b
lpatyn <lautaropatyn@outlook.com>
2023-04-27 18:44:34 -0300

    Prompt system created + added a little welcome mssg

---
+++ resources/prompts.js
@@ -0,0 +1,15 @@
+module.exports = {
+    hola: `Hola, %NAME%, todo bien?`,
+    adios: `Nos vemos %NAME%, cuidate!`,
+    nihongo: `私はボットのMoockyです, よろしくお願いします！`,
+    comandos: `Hola! Soy Moocky, tu bot otaku de confianza.
+
+A continuación te dejaré una lista de comandos útiles que puedes enviarme:
+
+    !hola - Te recibiré como corresponde.
+    !adios - Si te vas, no olvides despedirte de mí!
+    !nihongo - Pues claro, estamos en una academia de japonés, o no?
+    !comandos - Te mostraré nuevamente este mensaje guía.
+    
+Saludos!`
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 require('dotenv').config();
 const client = require('../resources/client');
 const channels = require('../resources/channels');
+const prompts = require('../resources/prompts');
+const promptArray = Object.keys(prompts).map(key => '!' + key);
 
 client.on('ready', (c) => {
     console.log(`Psst, ${c.user.username}'s online.`)
 });
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
-
+client.on('guildMemberAdd', (member) => {
     const welcomeChannel = client.channels.cache.get(channels.welcome);
-    welcomeChannel.send(`Que onda, ${message.author.username}`);
+
+    welcomeChannel.send(`Bienvenid@, ${member.user.username}!`);
+})
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot || !promptArray.includes(message.content)) return;
+    let reply;
+
+    switch (message.content) {
+        case '!hola':
+            reply = prompts.hola.replace('%NAME%', message.author.username);
+            break;
+        case '!adios':
+            reply = prompts.adios.replace('%NAME%', message.author.username);
+            break;
+        case '!nihongo':
+            reply = prompts.nihongo;
+            break;
+        case '!comandos':
+            reply = prompts.comandos;
+            break;
+    };
+
+    message.channel.send(reply);
 
 });
 
